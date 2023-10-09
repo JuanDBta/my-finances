6d75b81fb7d4b40a5305ae9bab76cb136c4fb39b
JuanDBta <simplebetsas@gmail.com>
2023-10-09 16:35:11 -0500

    Created migrations

---
+++ db/migrate/20231009213236_create_operations.rb
@@ -0,0 +1,12 @@
+class CreateOperations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :operations do |t|
+      t.string :name, null: false
+      t.decimal :amount, precision: 10, scale: 2,default: 0.00
+      t.references :category, null: false, foreign_key: true
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
