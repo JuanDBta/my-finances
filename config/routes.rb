0aec9680a6bd6d1c63d761e5d6f6243f696bde4d
JuanDBta <simplebetsas@gmail.com>
2023-10-09 16:43:38 -0500

    Added route

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'categories#index'
+  resources :categories, only: [:index, :new, :create]
+
 end
