b5a6390103842a7f16a597310ff68675a8508a86
JuanDBta <simplebetsas@gmail.com>
2023-10-12 12:24:55 -0500

    Fixed offenses

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Generate the file.
 
 class CategoriesController < ApplicationController
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # app/models/ability.rb
 # Generate the 'ability' file.
 class Ability
