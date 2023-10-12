01c11289f6d00a5975e8678f256cbc58dcf86ce3
JuanDBta <simplebetsas@gmail.com>
2023-10-12 15:47:11 -0500

    Added tests for models

--- spec/models/operation_spec.rb
+++ spec/models/operation_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe Operation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) do
+    User.create(
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: 'password123',
+      password_confirmation: 'password123'
+    )
+  end
+
+  let(:category) do
+    Category.new(
+      name: 'Fast Food',
+      icon: 'imgexample.png',
+      user:
+    )
+  end
+
+  it 'is valid with valid attributes' do
+    operation = Operation.new(
+      name: 'Burger',
+      amount: 1.5,
+      user:,
+      category:
+    )
+    expect(operation).to be_valid
+  end
+
+  describe 'associations' do
+    it 'belongs to user' do
+      operation = Operation.new(
+        name: 'Burger',
+        amount: 1.5,
+        user:,
+        category:
+      )
+      expect(operation.user).to eq(user)
+    end
+
+    it 'belongs to category' do
+      operation = Operation.new(
+        name: 'Burger',
+        amount: 1.5,
+        user:,
+        category:
+      )
+      expect(operation.category).to eq(category)
+    end
+  end
 end
